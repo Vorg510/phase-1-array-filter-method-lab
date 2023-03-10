d6f08115989b20c2e31c6e47c911eb33646e6981
Yuferril <yuferril.yunos@gmail.com>
2023-03-10 13:11:39 +1100

    commit tree 7cb999be7ef02e1d3fbaf918eab18b558b79b15f

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(names, name){
+    return names.filter(
+        (match) => match.toLowerCase() === name.toLowerCase()
+    )
+}
+
+function fuzzyMatch(names, search){
+    return names.filter(
+        (match) => match.toLowerCase().charAt(0) === search.toLowerCase().charAt(0)
+    )
+}
+
+function matchName(drivers, search){
+    return drivers.filter(
+        (match) => match.name === search
+    )
+}
